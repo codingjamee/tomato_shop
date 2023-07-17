29c75f68ace76221d9bf08d0439791d2ef2dd8da
codingjamee <codingjamee@gmail.com>
2023-07-17 15:23:59 +0900

    이미지 미리보기img-add-box 사이즈에 맞춰  구현하기

--- src/components/UI/ImgAdd.jsx
+++ src/components/UI/ImgAdd.jsx
@@ -1,22 +1,51 @@
-const ImgAdd = () => {
+import { useRef } from "react";
+
+const ImgAdd = (props) => {
+  const imgRef = useRef();
+
+  const saveImgView = () => {
+    const file = imgRef.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      props.setImgView(reader.result);
+    };
+  };
   return (
-    <div className="img-add-box">
-      <form className="img-add-box__form">
-        <p>이미지를 추가해주세요</p>
-        <div className="img-add-box__plus">
-          <span className="material-symbols-outlined">add_circle</span>
-        </div>
-        <label htmlFor="imgAdd" className="img-add-box__label btn">
-          업로드
-        </label>
-        <input
-          type="file"
-          accept="image/*"
-          style={{ display: "none" }}
-          id="imgAdd"
-        />
-      </form>
-    </div>
+    <>
+      <div className="img-add-box">
+        {props.imgView && (
+          <div className="img-view">
+            <p>미리보기</p>
+            <div className="img-view__wrapper">
+              <div className="img-view__img">
+                <img src={props.imgView} alt="" />
+              </div>
+              <button className="btn img-view__btn">삭제하기</button>
+            </div>
+          </div>
+        )}
+        {!props.imgView && (
+          <div className="img-add-box__wrapper">
+            <div className="img-add-box__plus">
+              <span className="material-symbols-outlined">add_circle</span>
+            </div>
+            <p>이미지를 추가해주세요</p>
+            <label htmlFor="imgAdd" className="img-add-box__label btn">
+              업로드
+            </label>
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              id="imgAdd"
+              onChange={saveImgView}
+              ref={imgRef}
+            />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,25 +4,23 @@ import ImgAdd from "../components/UI/ImgAdd";
 const Admin = () => {
   const [imgView, setImgView] = useState("");
   const [moreImgView, setMoreImgView] = useState("");
-  const imgRef = useRef();
   const moreImgRef = useRef();
 
-  const saveImgView = () => {
-    const file = imgRef.current.files[0];
-    const reader = new FileReader();
-    console.log(file);
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImgView(reader.result);
-    };
-  };
   return (
     <section className="content">
       <div className="content__wrapper">
         <form>
           <table className="table">
-            <thead>제품 등록하기</thead>
-            <tbody>
+            <colgroup>
+              <col className="table_col-1" width="20%" />
+              <col className="table_col-2" width="80%" />
+            </colgroup>
+            <thead className="table__head">
+              <tr>
+                <th colSpan={4}>제품 등록하기</th>
+              </tr>
+            </thead>
+            <tbody className="table__body">
               <tr className="table__row">
                 <td className="table__title">
                   <label htmlFor="productTitle">상품명</label>
@@ -38,36 +36,51 @@ const Admin = () => {
               <tr className="table__row">
                 <td className="table__title">기본이미지</td>
                 <td className="table__data">
-                  {imgView && (
-                    <div className="img-view">
-                      <img src={imgView} alt="" />
-                    </div>
-                  )}
-                  <ImgAdd />
+                  <div className="table__data--add">
+                    <ImgAdd imgView={imgView} setImgView={setImgView} />
+                  </div>
                 </td>
               </tr>
               <tr className="table__row">
                 <td className="table__title">
                   <label htmlFor="imgUrl">추가이미지</label>
                 </td>
-                <td className="table__data table__data--add">
-                  {imgView && (
-                    <div className="img-view">
-                      <img src={imgView} alt="" />
-                    </div>
-                  )}
-                  <ImgAdd />
-                  <ImgAdd />
-                  <ImgAdd />
-                  <ImgAdd />
+                <td className="table__data">
+                  <div className="table__data--add">
+                    <ImgAdd imgView={imgView} setImgView={setImgView} />
+                    <ImgAdd imgView={imgView} setImgView={setImgView} />
+                    <ImgAdd imgView={imgView} setImgView={setImgView} />
+                    <ImgAdd imgView={imgView} setImgView={setImgView} />
+                  </div>
+                </td>
+              </tr>
+              <tr className="table__row">
+                <td className="table__title">
+                  <label htmlFor="imgUrl">제품 설명</label>
+                </td>
+                <td className="table__data">
+                  <div className="table__textarea-wrapper">
+                    <textarea
+                      name=""
+                      id=""
+                      rows="10"
+                      className="table__textarea"
+                    ></textarea>
+                  </div>
                 </td>
               </tr>
             </tbody>
           </table>
+          <button
+            className="btn form__button"
+            type="submit"
+            onSubmit={() => {
+              console.log();
+            }}
+          >
+            등록하기
+          </button>
         </form>
-        <button className="btn form__button" type="submit">
-          등록하기
-        </button>
       </div>
     </section>
   );
