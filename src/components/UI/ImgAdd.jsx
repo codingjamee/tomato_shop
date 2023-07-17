7ef28a052fc266f13e7b843350b707303ac22993
codingjamee <codingjamee@gmail.com>
2023-07-17 15:39:12 +0900

    삭제하기 버튼 클릭시 미리보기 이미지 안보이게 하기

--- src/components/UI/ImgAdd.jsx
+++ src/components/UI/ImgAdd.jsx
@@ -12,40 +12,45 @@ const ImgAdd = (props) => {
     };
   };
   return (
-    <>
-      <div className="img-add-box">
-        {props.imgView && (
-          <div className="img-view">
-            <p>미리보기</p>
-            <div className="img-view__wrapper">
-              <div className="img-view__img">
-                <img src={props.imgView} alt="" />
-              </div>
-              <button className="btn img-view__btn">삭제하기</button>
+    <div className="img-add-box">
+      {props.imgView && (
+        <div className="img-view">
+          <p>미리보기</p>
+          <div className="img-view__wrapper">
+            <div className="img-view__img">
+              <img src={props.imgView} alt="" />
             </div>
+            <button
+              className="btn img-view__btn"
+              onClick={() => {
+                props.setImgView();
+              }}
+            >
+              삭제하기
+            </button>
           </div>
-        )}
-        {!props.imgView && (
-          <div className="img-add-box__wrapper">
-            <div className="img-add-box__plus">
-              <span className="material-symbols-outlined">add_circle</span>
-            </div>
-            <p>이미지를 추가해주세요</p>
-            <label htmlFor="imgAdd" className="img-add-box__label btn">
-              업로드
-            </label>
-            <input
-              type="file"
-              accept="image/*"
-              style={{ display: "none" }}
-              id="imgAdd"
-              onChange={saveImgView}
-              ref={imgRef}
-            />
+        </div>
+      )}
+      {!props.imgView && (
+        <div className="img-add-box__wrapper">
+          <div className="img-add-box__plus">
+            <span className="material-symbols-outlined">add_circle</span>
           </div>
-        )}
-      </div>
-    </>
+          <p>이미지를 추가해주세요</p>
+          <label htmlFor="imgAdd" className="img-add-box__label btn">
+            업로드
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            id="imgAdd"
+            onChange={saveImgView}
+            ref={imgRef}
+          />
+        </div>
+      )}
+    </div>
   );
 };
 
